e753a65b9711a016c3ffff15c497ab24bc647c2a
dimas <kabanooovd@yandex.ru>
2023-03-06 10:28:13 +0300

    06032023

--- src/config.ts
+++ src/config.ts
@@ -18,7 +18,7 @@ export const navigations: INavigations[] = [
   {
     id: uuid.v4(),
     route: ROUTES.MENU,
-    title: "Гравная",
+    title: "Главная",
   },
   {
     id: uuid.v4(),
@@ -30,11 +30,11 @@ export const navigations: INavigations[] = [
     route: ROUTES.EDUCATION,
     title: "Образование",
   },
-  {
-    id: uuid.v4(),
-    route: ROUTES.CONTACTS,
-    title: "Контакты",
-  },
+  // {
+  //   id: uuid.v4(),
+  //   route: ROUTES.CONTACTS,
+  //   title: "Контакты",
+  // },
   {
     id: uuid.v4(),
     route: ROUTES.ABOUT,
